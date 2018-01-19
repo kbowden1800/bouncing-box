927abc14d77265c23b3b30342875d8ec2ffa3ec6
Magdalene Mc <magdalener.mcarthur@gmail.com>
2018-01-19 15:22:14 -0600

    update test

--- test/index.spec.js
+++ test/index.spec.js
@@ -12,58 +12,51 @@ const should = window.should = chai.should();
 
 
 describe('Bouncing Box', function(){
-    /*
-      beforeEach(function(done) {
-        browser.visit(server, done);
-      });
-    */
+
       it('box variables and functions should exist', function(done){
         should.exist(position);
         should.exist(points);
         should.exist(speed);
-        should.exist(handleBoxClick());
+        should.exist(handleBoxClick);
         should.exist(update);
-               
       });
     
       it('box variables should have the correct values', function(){
-        expect(position).to.equal(0);
-        expect(points).to.equal(0);
-        expect(speed).to.equal(10);
-
-        
+        expect(position).to.eql(0);
+        expect(points).to.eql(0);
+        expect(speed).to.eql(10);    
       });
-    
+
+      it('should update position of box', function(){
+        var 
+          acceleration = speed * direction,
+          movement = position + acceleration;
+        expect(update).to.be.a('function');
+        update();
+        expect(position).to.eql(movement);
+      });
+
+      it('should bounce from left to right on the screen', function(){
+        update();
+        expect(position).to.satisfy(function(position){
+            if((position === 1)||(position === -1)){
+              return true;
+            } else {
+              return false;
+            }
+        });
+      });
+
       it('box should increase in speed once clicked', function(){
         expect(handleBoxClick).to.be.a('function');
         //save speed to a temp variable
-        //save points to a temp variable
-        //speed = startspeed + 3
+        var 
+          tempSpeed = speed + 3,
+          newPoints = points + 1;
         handleBoxClick();
-        //test for temp variable to equal speed + 3
-        expect(speed).to.equal(speed + 3);
-        expect(position).to.equal(0);
-        expect(points).to.equal(points + 1);
+        expect(speed).to.eql(tempSpeed);
+        expect(position).to.eql(0);
+        expect(points).to.eql(newPoints);
       });
-
-      it('box should bounce from each side of browser screen', function(){
-        expect(update).to.be.a('function');
-        update();
-        expect(position).to.equal(position + (speed * direction));
-        update();
-        expect(position).to.be.either(1).or(-1);    
-      });
-      
-      it('the box should increase speed when clicked',function(){
-        expect(handleBoxClick).to.be.a('function');
-        handleBoxClick();
-        should.exist(speed);
-        expect(speed).to.equal(speed + 3);
-        should.exist(position);
-        expect(position).to.equal(0);
-        should.exist(points);
-        expect(points).to.equal(points + 1);
-      });
-
-    });
+});
     
